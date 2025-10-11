bf83ab46b39d687ee6f3596eb9b574f3e2ba0148
Mohit Garg <mohitgarghnd825@gmail.com>
2025-10-11 18:55:21 +0530

    Add delete tweet functionality with security checks and route

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -23,8 +23,38 @@ const postTweet = asyncHandler(async(req,res) => {
 
 })
 
+const deleteTweet = asyncHandler(async(req,res) => {
+    const { tweetId } = req.params
+    if(!tweetId ){
+            throw new ApiError(400," tweetId not found for delete")
+    }
+    const tweet = await Tweet.findById(tweetId)
+    if(!tweet ){
+            throw new ApiError(400," tweet not found for delete")
+    }
+
+    
+    //  SECURITY CHECK: 
+    if (tweet.owner.toString() !== req.user?._id?.toString()) {
+        throw new ApiError(403, "Aap sirf apni hi tweet delete kar sakte hain.");
+    }
+
+    try{
+        await Tweet.findByIdAndDelete(tweetId)
+    }catch{
+        throw new ApiError(403,"tweet not delete successfully")
+    }
+
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(200,{},"tweet delete successfully" )
+    )
+})
+
 
 
 export {
     postTweet,
+    deleteTweet
 }
--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { postTweet } from "../controllers/tweet.controller.js";
+import { deleteTweet, postTweet } from "../controllers/tweet.controller.js";
 
 const router = Router()
 
 router.route("/postTweet").post(
     verifyJWT,
     postTweet
+)
+router.route("/deleteTweet/:tweetId").delete(
+    verifyJWT,
+    deleteTweet
 )
 
 export default router
