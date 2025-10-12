e4ab94cd01644e6b8b679d79fcaff9b70e35d887
Mohit Garg <mohitgarghnd825@gmail.com>
2025-10-12 14:49:38 +0530

    Add functionality to retrieve like counts for videos, tweets, and comments with appropriate validation

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -146,8 +146,232 @@ const tooglelikeComment = asyncHandler(async(req,res) => {
 
 })
 
+const getvideoLike = asyncHandler(async(req,res) => {
+    const { videoId } = req.params
+    if (!mongoose.Types.ObjectId.isValid(videoId)) {
+        throw new ApiError(400, "Invalid videoId format.");
+    }
+
+    const video = await Video.findById(videoId)
+    if(!video){
+        throw new ApiError(404,"Video not found")
+    }
+
+    const getlike = await Video.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(videoId)
+            }
+        },
+        {
+            $lookup: {
+                from: "likes",
+                localField: "_id",
+                foreignField: "video",
+                as:"likesDetails",
+                pipeline: [
+                    {
+                        $project: {
+                            //  IMPORTANT: Hum sirf likedBy user ki ID nikal rahe hain
+                            _id: 0, 
+                            likedBy: "$likedBy" 
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $addFields: {
+                no_ofLikesCount: {
+                    $size: "$likesDetails"
+                }
+            }
+        },
+        {
+            $project: {
+                no_ofLikesCount: 1,
+                tweetOwner: "$owner", 
+                likedByUsers: "$likesDetails.likedBy", 
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, getlike[0], "getvideoLike fetched successfully")
+    )
+})
+
+// const getTweetLike = asyncHandler(async(req,res) => {
+//     const { tweetId } = req.params
+//     if (!mongoose.Types.ObjectId.isValid(tweetId)) {
+//         throw new ApiError(400, "Invalid tweetId format.");
+//     }
+
+//     const tweet = await Tweet.findById(tweetId)
+//     if(!tweet){
+//         throw new ApiError(404,"tweet not found")
+//     }
+
+//     const getlike = await Tweet.aggregate([
+//         {
+//             $match: {
+//                 _id: new mongoose.Types.ObjectId(tweetId)
+//             }
+//         },
+//         {
+//             $lookup: {
+//                 from: "likes",
+//                 localField: "_id",
+//                 foreignField: "tweet",
+//                 as:"likesDetails",
+//                
+//             }
+//         },
+//         {
+//             $addFields: {
+//                 no_ofLikesCount: {
+//                     $size: "$likesDetails"
+//                 }
+//             }
+//         },
+//         {
+//             $project: {
+//                 no_ofLikesCount: 1,
+//                 tweetOwner: "$owner", 
+//                 likedByUsers: "$likesDetails", 
+//             }
+//         }
+//     ])
+
+//     return res
+//     .status(200)
+//     .json(
+//         new ApiResponse(200, getlike[0], "getTweetLike fetched successfully")
+//     )
+// })
+
+const getTweetLike = asyncHandler(async(req,res) => {
+    const { tweetId } = req.params
+    if (!mongoose.Types.ObjectId.isValid(tweetId)) {
+        throw new ApiError(400, "Invalid tweetId format.");
+    }
+
+    const tweet = await Tweet.findById(tweetId)
+    if(!tweet){
+        throw new ApiError(404,"tweet not found")
+    }
+
+    const getlike = await Tweet.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(tweetId)
+            }
+        },
+        {
+            $lookup: {
+                from: "likes",
+                localField: "_id",
+                foreignField: "tweet",
+                as:"likesDetails",
+                pipeline: [
+                    {
+                        $project: {
+                            //  IMPORTANT: Hum sirf likedBy user ki ID nikal rahe hain
+                            _id: 0, 
+                            likedBy: "$likedBy" 
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $addFields: {
+                no_ofLikesCount: {
+                    $size: "$likesDetails"
+                }
+            }
+        },
+        {
+            $project: {
+                no_ofLikesCount: 1,
+                tweetOwner: "$owner", 
+                likedByUsers: "$likesDetails.likedBy", 
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, getlike[0], "getTweetLike fetched successfully")
+    )
+})
+
+const getCommentLike = asyncHandler(async(req,res) => {
+    const { commentId } = req.params
+    if (!mongoose.Types.ObjectId.isValid(commentId)) {
+        throw new ApiError(400, "Invalid commentId format.");
+    }
+
+    const comment = await Tweet.findById(commentId)
+    if(!comment){
+        throw new ApiError(404,"comment not found")
+    }
+
+    const getlike = await Comment.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(commentId)
+            }
+        },
+        {
+            $lookup: {
+                from: "likes",
+                localField: "_id",
+                foreignField: "tweet",
+                as:"likesDetails",
+                pipeline: [
+                    {
+                        $project: {
+                            //  IMPORTANT: Hum sirf likedBy user ki ID nikal rahe hain
+                            _id: 0, 
+                            likedBy: "$likedBy" 
+                        }
+                    }
+                ]
+            }
+
+        },
+        {
+            $addFields: {
+                no_ofLikesCount: {
+                    $size: "$likesDetails"
+                }
+            }
+        },
+        {
+            $project: {
+                no_ofLikesCount: 1,
+                tweetOwner: "$owner", 
+                likedByUsers: "$likesDetails.likedBy", 
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, getlike[0], "getCommentLike fetched successfully")
+    )
+})
+
 export {
     tooglelikeComment,
     tooglelikeTweet,
-    tooglelikeVideo
+    tooglelikeVideo,
+    getvideoLike,
+    getTweetLike,
+    getCommentLike
 }
--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { tooglelikeComment, tooglelikeTweet, tooglelikeVideo } from "../controllers/like.controller.js";
+import { getCommentLike, getTweetLike, getvideoLike, tooglelikeComment, tooglelikeTweet, tooglelikeVideo } from "../controllers/like.controller.js";
+
 
 const router = Router()
 
@@ -18,5 +19,21 @@ router.route("/tweet/:tweetId").post(
     verifyJWT,
     tooglelikeTweet
 )
+
+router.route("/video/number_of_likes/:videoId").get(
+    verifyJWT,
+    getvideoLike
+)
+
+router.route("/tweet/number_of_likes/:tweetId").get(
+    verifyJWT,
+    getTweetLike
+)
+
+router.route("/comment/number_of_likes/:commentId").get(
+    verifyJWT,
+    getCommentLike
+)
+
 
 export default router
